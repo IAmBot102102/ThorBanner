9295fbb618e749545e61ffb1981bbd2c95af5159
IAmBot102102 <53452898+IAmBot102102@users.noreply.github.com>
2019-10-31 20:44:29 -0400

    Added Files

--- index.js
+++ index.js
@@ -1,1 +1,200 @@
-No u
+require("Discord.js")
+const token = "NjM5NjE5OTUzNDcyODMxNDg5.Xbt7YQ.xYwTlD4xLbLW90bZDwNAL7R9-38";
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+
+client.on("guildDelete", guild => {
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+
+});
+
+
+
+
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+
+  
+
+  // Also good practice to ignore any message that does not start with our prefix, 
+
+  // which is set in the configuration file.
+
+  if(message.content.indexOf(">") !== 0 || message.content.indexOf("!") !== 0) return;
+
+  
+
+  // Here we separate our "command" name, and our "arguments" for the command. 
+
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+
+  // command = say
+
+  // args = ["Is", "this", "the", "real", "life?"]
+
+  
+
+  // Let's go with a few common example commands! Feel free to delete or change those.
+
+  
+    var command = message.content;
+  if(command === ">ping" || command === "!ping" ) {
+
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+
+    const m = await message.channel.send("Ping?");
+
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+
+  }
+
+  
+
+
+  if(command.startsWith(">kick")){
+    let member = message.mentions.members.first();
+
+    if(!member)
+
+      return message.reply("Please mention a valid member of this server");
+    
+      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: I said so! :wave:`);
+  }
+
+  if(command.startsWith("!kick")) {
+
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+
+    // Please read on Array.some() to understand this bit: 
+
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+
+    if(!message.member.roles.some(r=>['Rubix'].includes(r.name)))
+
+      return message.reply("Sorry, you don't have permissions to use this!");
+
+    
+
+    // Let's first check if we have a member and if we can kick them!
+
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+
+    // We can also support getting the member by ID, which would be args[0]
+
+    let member = message.mentions.members.first();
+
+    if(!member)
+
+      return message.reply("Please mention a valid member of this server");
+
+    if(!member.kickable) 
+
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+
+    
+
+    // slice(1) removes the first part, which here should be the user mention or ID
+
+    // join(' ') takes all the various parts to make it a single string.
+    
+
+    // Now, time for a swift kick in the nuts!
+
+    await member.kick(reason)
+
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: I said so! :wave:`);
+
+
+
+  }
+
+  
+
+  if(command === "ban") {
+
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+
+    // In the real world mods could ban too, but this is just an example, right? ;)
+
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+
+      return message.reply("Sorry, you don't have permissions to use this!");
+
+    
+
+    let member = message.mentions.members.first();
+
+    if(!member)
+
+      return message.reply("Please mention a valid member of this server");
+
+    if(!member.bannable) 
+
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+
+
+    let reason = args.slice(1).join(' ');
+
+    if(!reason) reason = "No reason provided";
+
+    
+
+    await member.ban(reason)
+
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+
+  }
+
+  
+
+  if(command === "purge") {
+
+    // This command removes all messages from all users in the channel, up to 100.
+
+    
+
+    // get the delete count, as an actual number.
+
+    const deleteCount = parseInt(args[0], 10);
+
+    
+
+    // Ooooh nice, combined conditions. <3
+
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+
+    
+
+    // So we get our messages, and delete them. Simple enough, right?
+
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+
+    message.channel.bulkDelete(fetched)
+
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+
+  }
+
+});
